e27610141f526aad0918568e32437004590ed6d5
pk90264180 <pk90264180@gmail.com>
2023-08-09 14:00:10 +0530

    with env

--- db.js
+++ db.js
@@ -2,21 +2,21 @@ const mysql = require('mysql2');
 const dotenv = require('dotenv');
 dotenv.config();
 
-// const connection = mysql.createConnection({
-//   host: '127.0.0.1',
-//   port: 3306,
-//   user: 'root',
-//   password: 'kali',
-//   database: 'notary_server',
-// });
-
 const connection = mysql.createConnection({
-  host: process.env.host,
-  port: process.env.port,
-  user: process.env.user,
-  password: process.env.password,
-  database: process.env.database,
+  host: '127.0.0.1',
+  port: 3306,
+  user: 'root',
+  password: 'kali',
+  database: 'notary_server',
 });
+
+// const connection = mysql.createConnection({
+//   host: process.env.host,
+//   port: process.env.port,
+//   user: process.env.user,
+//   password: process.env.password,
+//   database: process.env.database,
+// });
 
 connection.connect((err) => {
   if (err) {
